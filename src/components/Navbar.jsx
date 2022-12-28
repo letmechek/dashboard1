9bd756b27eab26567c21ee7e1edeef7b45357454
letmechek <abdullahi.ali1017@gmail.com>
2022-12-28 12:26:42 -0800

    adds navbar icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,66 @@
-import React from 'react'
-import { LightModeOutlined, DarkModeOutlined, Menu as MenuIcon, Search, SettingsOutlined, ArrowDropDownOutlined } from '@mui/icons-material'
-import FlexBetween from './FlexBetween'
+import React, { useState } from "react";
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  Menu as MenuIcon,
+  Search,
+  SettingsOutlined,
+  ArrowDropDownOutlined,
+} from "@mui/icons-material";
+import FlexBetween from "./FlexBetween";
+import { useDispatch } from "react-redux";
+import  setMode  from "../state/index";
+// import profileImage from "assets/profile.jpeg";
+import { useTheme } from "@emotion/react";
+import { AppBar, Icon, IconButton, InputBase, Toolbar } from "@mui/material";
 
 const Navbar = () => {
+  const dispatch = useDispatch()
+  const theme = useTheme()
+
   return (
-    <div>Navbar</div>
+    <AppBar
+    sx={{
+      position: 'static',
+      background: 'none',
+      boxShadow: 'none',
+
+    }}
+    >
+      <Toolbar sx={{ justifyContent: 'space-between'}}
+      >
+        {/* LEFT SIDE  */}
+        <FlexBetween>
+          <IconButton onClick={() => console.log('open side bar')}>
+            <MenuIcon />
+          </IconButton>
+          <FlexBetween 
+          backgroundColor={theme.palette.background.alt}
+          borderRadius='9px'
+          gap='3rem'
+          p='0.1rem 1.5rem'
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        </FlexBetween>
+        {/* right side  */}
+        <FlexBetween gap='1.5rem'>
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === 'dark' ? (
+              <DarkModeOutlined sx={{ fontSize: '25px'}} />
+            ) : (
+              <LightModeOutlined sx={{ fontSize: '25px'}} />
+            )}
+          </IconButton>
+          <IconButton>
+            <SettingsOutlined sx={{ fontSize: '25px'}} />
+          </IconButton>
+        </FlexBetween>
+      </Toolbar>
+    </AppBar>
   )
 }
 
